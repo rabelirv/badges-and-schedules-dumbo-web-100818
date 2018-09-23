e5c5136a660840d155bb7dcde630e4fa7a40c1eb
Irving E Rabel <rabel606@gmail.com>
2018-09-23 23:17:57 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges=[]
+attendees.each {|attendee| badges<< badge_maker(attendee)}
+return badges
+end 
+
+def assign_rooms(attendees)
+ rooms= 1
+  attendees.collect {|attendee| room_assignments<< "Hello, #{attendee}! You'll be assigned to room #{rooms}!"
+    rooms +=1
+  }
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees)
+  assign_rooms(attendees)
+end
